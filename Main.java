7e9ad3f23081a84be652f05fdb3e7e71deb950f1
Rohit-Jadhav-45 <120640822+Rohit-Jadhav-45@users.noreply.github.com>
2022-12-15 15:24:47 +0530

    New Files Uploaded

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		AdminDepartment subAdmin = new AdminDepartment();
+		HrDepartment subHr = new HrDepartment();
+		TechDepartment subTech = new TechDepartment();
+
+		System.out.println("Welcome to " + subAdmin.departmentName() + "\n" + subAdmin.getTodaysWork() + "\n"
+				+ subAdmin.getWorkDeadline() + "\n" + subAdmin.isTodayAHoliday() + "\n");
+		System.out.println("Welcome to " + subHr.departmentName() + "\n" + subHr.doActivity() +"\n" + subHr.getTodaysWork() + "\n"
+				+ subHr.getWorkDeadline() + "\n" + subHr.isTodayAHoliday() + "\n");
+		System.out.println("Welcome to " + subTech.departmentName() + "\n" + subTech.getTodaysWork() + "\n"
+				+ subTech.getWorkDeadline() + "\n" + subTech.getTechStackInformation() + "\n"
+				+ subTech.isTodayAHoliday() + "\n");
+
+	}
+
+}
---
+++ SuperDepartment.java
@@ -0,0 +1,23 @@
+public class SuperDepartment {
+
+	protected String departmentName() {
+
+		return "Super Department";
+	}
+
+	public String getTodaysWork() {
+
+		return "No Work as of now";
+	}
+
+	public String getWorkDeadline() {
+
+		return "Nil";
+	}
+
+	public String isTodayAHoliday() {
+
+		return "Today is not a holiday";
+	}
+
+}
